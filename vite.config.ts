32fde75caccf3598fb9e8eb1cf713e038dca0c8e
Oscar Bautista <oscar04262000@gmail.com>
2024-10-07 19:17:47 -0600

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/personal-web-page/'
+  base : "https://oscar-BG.github.io/personal-web-page"
 })
