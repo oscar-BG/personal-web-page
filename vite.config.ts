a8d9f109d1a09e330a86188222f614ea36c23d04
Oscar Bautista Gaytan <oscar.bautista@criteria.mx>
2026-06-11 17:58:07 -0600

    Refactor: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base : "https://oscar-BG.github.io/personal-web-page"
+  base: "/personal-web-page/"
 })
